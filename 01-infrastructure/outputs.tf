db13b704142104b56601599f5a2f417f0a42265f
SerhiiMyronets <hardmail88@gmail.com>
2025-07-17 18:45:10 -0700

    ref

--- 01-infrastructure/outputs.tf
+++ 01-infrastructure/outputs.tf
@@ -3,16 +3,6 @@ output "cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = module.eks.cluster_name
-}
-
-output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc.vpc_id
-}
-
 output "aws_connection_command" {
   description = "command to connect to the EKS cluster"
   value       = "aws eks update-kubeconfig --region ${var.region} --name ${module.eks.cluster_name}"
